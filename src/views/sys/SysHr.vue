b2bdb138b64ac4b0c83cc8e6cfb77e8a90809db2
panlinglong <1136249418@qq.com>
2020-09-18 21:56:44 +0800

    员工1

--- src/views/sys/SysHr.vue
+++ src/views/sys/SysHr.vue
@@ -1,8 +1,51 @@
 <template>
     <div>
+      <el-dialog
+        :title="title"
+        :visible.sync="dialogVisible"
+        width="70%">
+        <div>
+          <el-form :model="hr" :rules="rules" ref="hrForm">
+            <el-row :gutter="20">
+              <el-col :span="8">
+                <el-form-item label="姓名：" prop="name">
+                  <el-input  style="width:150px" placeholder="请输入姓名" v-model="hr.name"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="联系方式：" prop="phone">
+                  <el-input  style="width:150px" placeholder="请输入联系方式" v-model="hr.phone"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="地址：" prop="address">
+                  <el-input  style="width:150px" placeholder="请输入地址" v-model="hr.address"></el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-row :gutter="20">
+              <el-col :span="8">
+                <el-form-item label="用户名：" prop="username">
+                  <el-input  style="width:150px" placeholder="请输入用户名" v-model="hr.username"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="8">
+                <el-form-item label="备注：" prop="remark">
+                  <el-input  style="width:150px" placeholder="请输入备注" v-model="hr.remark"></el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-form>
+        </div>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="doAddHr">确 定</el-button>
+        </span>
+      </el-dialog>
       <div style="margin-top: 10px;display: flex;justify-content: center">
         <el-input v-model="keywords" size="small" placeholder="通过用户名搜索用户..." prefix-icon="el-icon-search" style="width: 400px;margin-right: 10px" @keydown.enter.native="initHrs"></el-input>
         <el-button size="small" icon="el-icon-search" type="primary" @click="initHrs">搜索</el-button>
+        <el-button icon="el-icon-plus" type="primary" @click="showAddHrView">添加员工</el-button>
       </div>
       <div class="hr-container">
         <el-card class="hr-card" v-for="(hr,index) in hrs" :key="index">
@@ -20,7 +63,9 @@
                 @change="enabledChange(hr)"
                 active-text="启用"
                 inactive-text="禁用">
-              </el-switch></div>
+              </el-switch>
+
+              </div>
               <div>用户角色：<el-tag type="success" style="margin-right: 4px" v-for="(role,index) in hr.roles" :key="index">{{role.nameZh}}</el-tag><el-popover
                 placement="right"
                 title="角色列表"
@@ -40,28 +85,67 @@
               </el-popover>
                 </div>
               <div>备注：{{hr.remark}}</div>
+
+                <el-button
+                  @click.native.prevent="showHr"
+                  type="primary">
+                  编辑
+                </el-button>
+
             </div>
+
           </div>
+
         </el-card>
       </div>
     </div>
 </template>
 
+
 <script>
   export default {
-    name: '',
+    name: 'SysHr',
     data(){
       return{
+        title:'',
         keywords:'',
-        hrs:[],
+        hr: {
+          name:"",
+          phone:"",
+          address:"",
+          username:"",
+          remark:"",
+        },
+        hrs:[{
+
+        }],
         selectedRoles:[],
         allroles:[],
+        visible:false,
+        dialogVisible:false,
+        defaultProps: {
+          children: 'children',
+          label: 'name'
+        },
+        rules: {
+          name: [{required: true, message: '请输入姓名', trigger: 'blur'}],
+        },
       }
     },
     mounted(){
-      this.initHrs()
+      this.initHrs();
     },
     methods:{
+      emptyHr(){
+        this.hr={
+          name:"",
+          phone:"",
+          address:"",
+          username:"",
+          remark:"",
+
+        }
+      },
       initHrs(){
         this.getRequest("/system/basic/hr/?keywords="+this.keywords).then(res=>{
           this.hrs = res;
@@ -92,6 +176,43 @@
             this.initHrs();
           }
         })
+      },
+      doAddHr(){
+        if (this.hr.id){
+          this.$refs['hrForm'].validate(valid=>{
+            if (valid){
+              this.putRequest("/system/basic/hr/",this.hr).then(res=>{
+                this.dialogVisible = false;
+                this.initHrs();
+              })
+            }
+          })
+        }else {
+          this.$refs['hrForm'].validate(valid=>{
+            if (valid){
+              this.postRequest("/system/basic/hr/",this.hr).then(res=>{
+                this.dialogVisible = false;
+                this.initHrs();
+              })
+            }
+          })
+        }
+      },
+      showAddHrView(){
+        this.title="添加员工"
+        if(this.$refs['hrForm']!=undefined){
+          this.$refs['hrForm'].resetFields()
+        }
+        this.emptyHr();
+        this.dialogVisible=true;
+      },
+      showHr(row){
+        this.title='编辑员工信息';
+        if(this.$refs['hrForm']!=undefined){
+          this.$refs['hrForm'].resetFields()
+        }
+        this.hr=row;
+        this.dialogVisible=true;
       },
       enabledChange(hr){
         this.putRequest("/system/basic/hr/",hr).then(res=>{
